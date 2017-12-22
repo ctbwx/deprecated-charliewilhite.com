2d8c5c1cde6c2a58422066a83993a27ee2deb80b
Charlie <ctbwilhite@gmail.com>
2017-12-22 15:31:34 -0600

    Added a whole bunch of components.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="navbar">
+    <h1>header will go here</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+};
+</script>
+
+<style>
+.navbar {
+  text-align: center;
+  height: 200px;
+  margin-top: 10%;
+  margin-left: 20%;
+  margin-right: 20%;
+  font-family: system-ui;
+  border-color: rgb(129, 73, 251);
+  border-style: solid;
+}
+</style>
