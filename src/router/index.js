5c348db09b2ca88e2fe669f981be35c36ccdd556
Charlie <ctbwilhite@gmail.com>
2017-12-22 15:31:05 -0600

    Began to implement file structure as well as got the header compenent rendered. Next step: implement element ui

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Navbar from '@/components/Navbar';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Navbar',
+      component: Navbar,
     },
   ],
 });
