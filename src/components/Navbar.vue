4d7aacd87505c812f41bf1c317f4a9a3b5663d5d
Charlie <ctbwilhite@gmail.com>
2017-12-24 14:39:02 -0600

    Got paralllax to work in the header

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,27 +1,25 @@
-<template>
-  <div class="navbar">
-    <div class="comic">
-      <h1>{{ input }}</h1>
-      <el-input placeholder="Please input" v-model="input"></el-input>
-    </div>
-    <div class="buttons">
-      <el-button class="projects" type="primary" icon="el-icon-menu">Projects</el-button>
-      <el-button class="resume" type="primary" icon="el-icon-tickets">Résumé</el-button>
-      <el-button class="about-me" type="primary" icon="el-icon-more-outline">About Me</el-button>
-    </div>
-  </div>
+<template lang="pug">
+  div
+    div(class='parallax')
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      input: '',
-    };
-  },
 };
 </script>
 
-<style lang="sass">
+<style lang="scss">
+.parallax {
+    /* The image used */
+    background-image: url('../assets/drmanhattan.jpg');
 
+    /* Set a specific height */
+    height: 400px;
+    margin: 2%;
+
+    /* Create the parallax scrolling effect */
+    background-attachment: fixed;
+    background-position: top;
+    background-repeat: no-repeat;
+}
 </style>
