eafd8a8b7fe338b3add35b9037db4b4cfc110ff6
Charlie <ctbwilhite@gmail.com>
2018-01-03 16:59:40 -0600

    Changing layout of app. *Pivot*

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="app">
-    <navbar></navbar>
     <router-view/>
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar';
-
 export default {
   name: 'app',
-  components: { navbar: Navbar },
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import About from '@/components/AboutMe';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/aboutme',
-      name: 'About Me',
-      component: About,
-    },
   ],
 });
