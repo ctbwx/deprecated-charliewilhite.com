f7627f4ee9e9581941b6f711265d296099058b85
Charlie <ctbwilhite@gmail.com>
2018-01-04 16:29:39 -0600

    Added image assets

---
+++ src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+div
+    div(class='navbar')
+        span(class='header') Charlie Wilhite
+            div(class='social')
+                span(class='follow-sub') Follow me...
+                a(href='https://github.com/simplenclean' target='_blank'): img(src='../assets/github.png')
+                a(href='https://www.instagram.com/simplenclean_/' target='_blank'): img(src='../assets/instagram.png')
+                a(href='https://www.linkedin.com/in/charliewilhite/' target='_blank'): img(src='../assets/linkedin.png')
+            <el-tabs v-model="activeName" @tab-click="handleClick">
+                <el-tab-pane label="Blog" name="first"></el-tab-pane>
+                <el-tab-pane label="Projects" name="second"></el-tab-pane>
+                <el-tab-pane label="About Me" name="third"></el-tab-pane>
+            </el-tabs>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+.navbar {
+    margin: auto;
+    text-align: center;
+    transform: translateY(25%);
+    left: 0;
+    right: 0;
+    .header {
+        position: relative;
+        top: 10%;
+        font-family: Source Sans Pro, Helvetica Neue, Arial,sans-serif;
+        text-transform: uppercase;
+        letter-spacing: 0.26em;
+        font-weight: 550;
+        font-size: 20px;
+    }
+    .social {
+        .follow-sub {
+            position: relative;
+            vertical-align: 30%;
+        }
+        letter-spacing: 0.20em;
+        font-weight: 300;
+        font-size: 10px;
+        color:rgb(182, 182, 182);
+        position: fixed;
+        right: 1%;
+        top: 1%;
+        img {
+            margin: 3px;
+            width: 27px;
+            height: 27px;
+        }
+    }
+}
+</style>
