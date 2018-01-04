6fd698f6b40971c6f7b6e250cfa8adc81bfa023b
Charlie <ctbwilhite@gmail.com>
2018-01-04 16:25:13 -0600

    Finished bare min for navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="app">
+    <navbar/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar';
+
 export default {
   name: 'app',
+  components: {
+    navbar: Navbar,
+  },
 };
 </script>
