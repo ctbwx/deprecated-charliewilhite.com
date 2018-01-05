920756cf04f1277aa4a6c9987a717c221a0d7cf2
Charlie <ctbwilhite@gmail.com>
2018-01-05 01:40:53 -0600

    Optimized Navbar for mobile.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,7 +7,7 @@ div
                 a(href='https://github.com/simplenclean' target='_blank'): img(src='../assets/github.png')
                 a(href='https://www.instagram.com/simplenclean_/' target='_blank'): img(src='../assets/instagram.png')
                 a(href='https://www.linkedin.com/in/charliewilhite/' target='_blank'): img(src='../assets/linkedin.png')
-            <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tabs class='tabs' v-model='activeName' @tab-click='handleClick'>
                 <el-tab-pane label="Blog" name="first"></el-tab-pane>
                 <el-tab-pane label="Projects" name="second"></el-tab-pane>
                 <el-tab-pane label="About Me" name="third"></el-tab-pane>
@@ -40,37 +40,49 @@ export default {
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
 .navbar {
-    margin: auto;
-    text-align: center;
-    transform: translateY(25%);
-    left: 0;
-    right: 0;
-    .header {
-        position: relative;
-        top: 10%;
-        font-family: Source Sans Pro, Helvetica Neue, Arial,sans-serif;
-        text-transform: uppercase;
-        letter-spacing: 0.26em;
-        font-weight: 550;
-        font-size: 20px;
+  margin: auto;
+  text-align: center;
+  transform: translateY(25%);
+  left: 0;
+  right: 0;
+  .header {
+    position: relative;
+    top: 10%;
+    font-family: Source Sans Pro, Helvetica Neue, Arial,sans-serif;
+    text-transform: uppercase;
+    letter-spacing: 0.26em;
+    font-weight: 550;
+    font-size: 20px;
+  }
+  .social {
+    .follow-sub {
+      position: relative;
+      vertical-align: 30%;
     }
-    .social {
-        .follow-sub {
-            position: relative;
-            vertical-align: 30%;
-        }
-        position: fixed;
-        letter-spacing: 0.20em;
-        font-weight: 300;
-        font-size: 10px;
-        color:rgb(182, 182, 182);
-        right: 1%;
-        top: 1%;
-        img {
-            margin: 3px;
-            width: 27px;
-            height: 27px;
-        }
+    @media screen and (max-width: 500px) {
+      .follow-sub {
+        display: none;
+      }
     }
+    position: fixed;
+    letter-spacing: 0.20em;
+    font-weight: 300;
+    font-size: 10px;
+    color:rgb(182, 182, 182);
+    right: 1%;
+    top: 1%;
+    img {
+        margin: 3px;
+        width: 27px;
+        height: 27px;
+    }
+    @media screen and (max-width: 500px) {
+      img {
+        margin: 3px;
+        width: 17px;
+        height: 17px;
+      }
+    }
+  }
 }
 </style>
