c7887814f36aa32f2c9889bd9ab9b433482e2e02
Charlie <ctbwilhite@gmail.com>
2018-01-05 03:07:10 -0600

    Made server changes.

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('*', (req, res) => {
 });
 /*----------------------------------------------------------------------------------------------*/
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 server.listen(port);
 
 console.log(`Server listening on ${port}`);
