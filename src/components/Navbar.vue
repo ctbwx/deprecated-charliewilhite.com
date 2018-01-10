6267b6bd70d901de3743177ecc6139432ab508d1
Charlie <ctbwilhite@gmail.com>
2018-01-10 17:42:36 -0600

    Polished navbar and about me

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,17 @@
 <template lang="pug">
 div
-    div(class='navbar')
-        span(class='header') Charlie Wilhite
-            <el-tabs v-model='activeName' @tab-click='handleClick'>
-                <el-tab-pane label="Blog" name="first"></el-tab-pane>
-                <el-tab-pane label="Projects" name="second"></el-tab-pane>
-                <el-tab-pane label="About Me" name="third"></el-tab-pane>
-            </el-tabs>
-            div(class='social')
-                span(class='follow-sub') Follow me...
-                a(href='https://github.com/simplenclean' target='_blank'): img(src='../assets/github.png')
-                a(href='https://www.instagram.com/simplenclean_/' target='_blank'): img(src='../assets/instagram.png')
-                a(href='https://www.linkedin.com/in/charliewilhite/' target='_blank'): img(src='../assets/linkedin.png')
+  div(class='navbar')
+    span(class='header' v-on:click='header') Charlie Wilhite
+    <el-tabs v-model='activeName' @tab-click='handleClick'>
+      <el-tab-pane label="Blog" name="first"></el-tab-pane>
+      <el-tab-pane label="Projects" name="second"></el-tab-pane>
+      <el-tab-pane label="About Me" name="third"></el-tab-pane>
+    </el-tabs>
+    div(class='social')
+      span(class='follow-sub') Follow me...
+      a(href='https://github.com/simplenclean' target='_blank'): img(src='../assets/github.png')
+      a(href='https://www.instagram.com/simplenclean_/' target='_blank'): img(src='../assets/instagram.png')
+      a(href='https://www.linkedin.com/in/charliewilhite/' target='_blank'): img(src='../assets/linkedin.png')
 </template>
 
 <script>
@@ -33,6 +33,9 @@ export default {
         this.$router.push('aboutme');
       }
     },
+    header() {
+      this.$router.push('/blog');
+    },
   },
 };
 </script>
@@ -40,19 +43,51 @@ export default {
 
 @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
 .navbar {
+  font-family: Source Sans Pro, Helvetica Neue, Arial,sans-serif;
   margin: auto;
   text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 0.20em;
   transform: translateY(25%);
   left: 0;
   right: 0;
+  -webkit-backface-visibility: hidden;
+  -webkit-transform: translateZ(0) scale(1.0, 1.0);
+  transform: translateZ(0);
   .header {
     position: relative;
-    top: 10%;
-    font-family: Source Sans Pro, Helvetica Neue, Arial,sans-serif;
-    text-transform: uppercase;
-    letter-spacing: 0.26em;
-    font-weight: 590;
+    top: 13px;
+    transform: translateY(50%);
     font-size: 22px;
+    font-weight: 590;
+    cursor: pointer;
+    color: #000;
+    text-decoration: none;
+  }
+  @media screen and (max-width: 500px) {
+    top: 9px;
+  }
+  .header:hover {
+    color: #000;
+  }
+  .header:before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background-color: #000;
+    visibility: hidden;
+    -webkit-transform: scaleX(0);
+    transform: scaleX(0);
+    -webkit-transition: all 0.3s ease-in-out 0s;
+    transition: all 0.3s ease-in-out 0s;
+  }
+  .header:hover:before {
+    visibility: visible;
+    -webkit-transform: scaleX(1);
+    transform: scaleX(1);
   }
   .social {
     .follow-sub {
@@ -69,8 +104,8 @@ export default {
     font-weight: 300;
     font-size: 10px;
     color:rgb(182, 182, 182);
-    right: 1%;
-    top: 1%;
+    right: 0;
+    top: 30%;
     img {
         margin: 3px;
         width: 27px;
