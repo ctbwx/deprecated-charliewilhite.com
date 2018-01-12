06d926fe8ad0522263c52789acd57f73558e37d1
Charlie <ctbwilhite@gmail.com>
2018-01-12 16:46:48 -0600

    Started adding blog-list component code and styling

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,15 +1,14 @@
 <template lang="pug">
-  div(class='wrapper')
-    ul(class='blog-list')
-      li(v-for='item in blogs') {{ item.title }}
-  
+  div(class='blog')
+    el-container(class='entry' v-for='item in blogs' :key='item.id')
+      el-header(class='header') {{ item.title }}
+      el-main(class='main') {{ item.teaser }}
+      el-footer(class='footer') {{ item.createdAt }}
 </template>
 
 <script>
 /* eslint arrow-body-style: ["error", "always"] */
 import data from '@/assets/data';
-
-console.log(data.blog);
 
 export default {
   data: () => {
@@ -21,5 +20,30 @@ export default {
 </script>
 
 <style lang="scss">
-
+  .blog {
+    display: flex;
+    width: 50%;
+    margin: auto;
+    padding-top: 5%;
+    left: 0;
+    right: 0;
+    justify-content: space-around;
+    flex-direction: column;
+  }
+  .entry {
+    margin: 5%;
+    border-color: orange;
+    border-style: solid;
+  }
+  .header {
+    text-align: center;
+    padding-top: 3%;
+    font-family: Source Sans Pro, Helvetica Neue, Arial,sans-serif;
+  }
+  .main {
+    font-family: Merriweather, Georgia, serif;
+  }
+  .footer {
+    font-family: Source Sans Pro, Helvetica Neue, Arial,sans-serif;
+  }
 </style>
