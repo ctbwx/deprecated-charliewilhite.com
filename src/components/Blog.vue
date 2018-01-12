861ed619866afaa36194a481b366980f82286ba5
Charlie <ctbwilhite@gmail.com>
2018-01-12 17:45:50 -0600

    Got all blog posts rendering, working on styling rn

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,9 +1,12 @@
 <template lang="pug">
   div(class='blog')
     el-container(class='entry' v-for='item in blogs' :key='item.id')
-      el-header(class='header') {{ item.title }}
-      el-main(class='main') {{ item.teaser }}
-      el-footer(class='footer') {{ item.createdAt }}
+      el-container
+        el-header(class='blog-header') {{ item.title }}
+        el-main(class='main') {{ item.teaser }}
+        el-footer(class='footer-text') {{ item.createdAt }}
+      //- el-aside(class='aside' v-if='item.thumbnail.length')
+      //-   img(class='aside-image' :src='item.thumbnail')
 </template>
 
 <script>
@@ -22,7 +25,7 @@ export default {
 <style lang="scss">
   .blog {
     display: flex;
-    width: 50%;
+    width: 60%;
     margin: auto;
     padding-top: 5%;
     left: 0;
@@ -32,18 +35,36 @@ export default {
   }
   .entry {
     margin: 5%;
-    border-color: orange;
-    border-style: solid;
   }
-  .header {
+  .blog-header {
+    display: block;
     text-align: center;
-    padding-top: 3%;
-    font-family: Source Sans Pro, Helvetica Neue, Arial,sans-serif;
+    font-size: 2em;
+    text-rendering: optimizelegibility;
+    font-style: italic;
+    font-weight: 300;
+    font-size: 43px;
+    line-height: 1.3em;
   }
   .main {
+    flex: 2;
+    font-style: normal;
+    font-variant-ligatures: normal;
+    font-variant-caps: normal;
+    font-variant-numeric: normal;
+    font-variant-east-asian: normal;
+    font-weight: normal;
+    font-stretch: normal;
+    font-size: 15px;
+    line-height: 1.9em;
     font-family: Merriweather, Georgia, serif;
   }
-  .footer {
+  .footer-text {
+    line-height: 100px;
+    bottom: 0px;
+    text-align: left;
     font-family: Source Sans Pro, Helvetica Neue, Arial,sans-serif;
+    font-size: 12px;
+    color: rgb(182, 182, 182);
   }
 </style>
