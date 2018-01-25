64492b3243dd0346c074e4e3982255905ac84ff1
Charlie <ctbwilhite@gmail.com>
2018-01-24 18:26:00 -0600

    Adding db setup

---
+++ herokuconfig.js
@@ -0,0 +1,9 @@
+require('dotenv').config({ path: '../env.env' });
+
+module.exports = {
+  host: 'ec2-184-73-175-95.compute-1.amazonaws.com',
+  user: 'wpendjwzlaqsfx',
+  password: '277180fb1d300e74285ddf2523ad656c359becabc83ea33d208e56bf7432c563',
+  ssl: true,
+  database: 'd70o4haqus5vgb'
+}
