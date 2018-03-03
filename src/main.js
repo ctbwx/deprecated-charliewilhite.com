0a5745b97fcf1376173277efb5f46937a009d848
simplenclean <ctbwilhite@gmail.com>
2018-03-02 19:29:29 -0600

    Fixed mobile margin on project component

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import VueClipboard from 'vue-clipboard2';
 import 'element-ui/lib/theme-chalk/index.css';
-import App from './app';
+import App from './App';
 import router from './router/index';
 
 Vue.config.productionTip = false;
